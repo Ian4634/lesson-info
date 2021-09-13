253eca36496dc52565c4d47b5d18028b5d0986af
Ian <flyingiancheng@gmail.com>
2021-09-13 15:12:15 +0800

    edited the official_use home page

--- official_use/url.py
+++ official_use/url.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('add_lesson/', views.add_lesson, name = 'add_lesson'),
     path('name/', views.name, name = 'name',),
     path('add_hashtag/', views.add_hashtag, name = 'add_hashtag'),
-    path('', views.official_home, name = 'official_home')
+    path('', views.official_home, name = 'official_home'), 
+    path('delete_lesson/<str:lesson_id>', views.delete_lesson, name = 'delete_lesson'),
+    path('delete_hashtag/', views.delete_hashtag, name = 'delete_hashtag')
 ]
--- official_use/views.py
+++ official_use/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .forms import LessonForm, CategoryForm
-from .models import Lesson
+from .models import Lesson, Category
+from mainpages import my_funcs
 from django.http import HttpResponseRedirect, HttpResponse
 # Create your views here.
 
@@ -12,7 +14,7 @@ def add_lesson(request):
         form.save()
         return HttpResponseRedirect('?submitted=True')
     elif 'submitted' in request.GET:
-        return HttpResponse('form sent')
+        return redirect(reverse('official_use:official_home'))
 
     else:
         form = LessonForm
@@ -28,8 +30,40 @@ def add_hashtag(request):
         form.save()
         return HttpResponseRedirect('?submitted=True')
     elif 'submitted' in request.GET:
-        return HttpResponse('form sent')
+        return redirect(reverse('official_use:official_home'))
 
     else:
         form = CategoryForm
         return render(request, 'add_hashtag.html', {'form':form})
+
+def official_home(request):
+    categories = [ category.name for category in Category.objects.all()]
+    if 'filter' in request.GET:
+        for category in categories:
+            if request.GET.get('filter') == category:
+                context = {
+                    'lessons':my_funcs.get_data(category),
+                    'category':category,
+                }
+                return render(request, 'home.html', context)
+    
+    lessons = Lesson.objects.all().order_by('start_time')
+    lesson_length = len(lessons)
+    context = {
+        'lessons':lessons,
+        'lesson_length':lesson_length,
+        'categories':categories,
+    }
+    return render(request, 'official_home.html', context)
+
+def delete_lesson(request, lesson_id):
+    Lesson.objects.get(id = lesson_id).delete()
+    return redirect(reverse('official_use:official_home'))
+
+def delete_hashtag(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        Category.objects.get(name = name).delete()
+        return redirect(reverse('official_use:official_home'))
+    else:
+        return render(request, 'delete_hashtag.html')
